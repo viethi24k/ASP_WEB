0f89cbdb9641fbedc30b92a76216fffda9a29382
ThanhViet <vietgreat2@gmail.com>
2024-09-13 17:31:54 +0700

    baitap06

---
+++ baitap06ltwed/Controllers/taikhoancontroller.cs
@@ -0,0 +1,21 @@
+﻿using baitap06ltwed.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace baitap06ltwed.Controllers
+{
+    public class taikhoancontroller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DangKi(TaiKhoanViewModel taikhoan)
+        {
+            if (taikhoan != null && taikhoan.Password != null && taikhoan.Password.Length > 0)
+            {
+                taikhoan.Password = taikhoan.Password + "_da_ma_hoa";
+            }
+            return View(taikhoan);
+        }
+    }
+}
---
+++ baitap06ltwed/Models/TaiKhoanViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace baitap06ltwed.Models
+{
+
+
+
+    public class TaiKhoanViewModel
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
