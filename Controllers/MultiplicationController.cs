5a18f9689aa0d50bb2a99165730dcbdca19b1cd9
ThanhViet <vietgreat2@gmail.com>
2024-09-14 09:09:37 +0700

    bai2

---
+++ Controllers/MultiplicationController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MultiplicationTableApp.Services;
+
+namespace MultiplicationTableApp.Controllers
+{
+    public class MultiplicationController : Controller
+    {
+        private readonly MultiplicationTableService _service;
+
+        public MultiplicationController(MultiplicationTableService service)
+        {
+            _service = service;
+        }
+
+        public IActionResult Index()
+        {
+            var table = _service.GetMultiplicationTable();
+            return View(table);
+        }
+    }
+}
